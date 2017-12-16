62a88c0fbdccdacf4cdc0b9865760766d5a5b9ef
Anaibi <burgostatiana@yahoo.com>
2017-12-16 18:05:58 +0100

    added code animation and update modules search box

---
+++ src/js/code-output-animation.js
@@ -0,0 +1,6 @@
+$(function() {
+    var code = $('#code-output');
+    var text = code.find('pre');
+
+    console.log(text);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,55 @@
 $(function() {
 
+  // update selected link in main navigation bar
+  var page = window.location.pathname.split('/');
 
+  page = page[page.length-1];
+
+  $('#main-nav').find('a[href="'+page+'"]').addClass('selected');
+
+
+  // update selected link in main navigation bar
+  // when in documentation pages = 
+  // * quickstart
+  // * guide
+  // * api
+  // * examples
+  // @TODO pass as array?
+
+  if (page === ("quickstart.html" || "guide.html" || "api.html" || "examples.html")) {
+    $('header').find('a[href="documentation.html"]').addClass('selected');
+  }
+
+  
+  // update documentation menu in documentation page
+
+  if (page === "documentation.html") {
+    var active = $('.content .main-txt').attr('id');
+    $('#docs-nav').find('a[href="'+active+'.html"]').addClass('selected');
+  }
+
+  
+  // update documentation link in main navigation bar
+  // when submenu is open
+  $('#main-nav .submenu').hover(function() {
+    $(this).closest('.menu-item').find('a').toggleClass('selected');
+  }); 
+
+
+  // add scrolling effects to styleguide:
+  // * to-top button
+  $("#to-top").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $('header').offset().top
+    }, 1000);
+  });
+
+  //  * styleguide menu
+  $("#styleguide-menu a").click(function() {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+  });
 
 });
